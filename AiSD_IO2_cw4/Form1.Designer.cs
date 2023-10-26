5f2bb9b201a54452095da90b0cda8470a4819dde
kacper0276 <72521677+kacper0276@users.noreply.github.com>
2023-10-26 16:26:23 +0200

    Ćwiczenia z dnia 26_10_2023

---
+++ AiSD_IO2_cw4/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace AiSD_IO2_cw4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.wynik = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.generateNumber = new System.Windows.Forms.NumericUpDown();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.sortInsert = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.separatorSelected = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.generateNumber)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 143);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(221, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "SB";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // wynik
+            // 
+            this.wynik.Location = new System.Drawing.Point(165, 274);
+            this.wynik.Name = "wynik";
+            this.wynik.Size = new System.Drawing.Size(100, 23);
+            this.wynik.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(140, 351);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "SS";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(316, 145);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(136, 19);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "Lewa strona aktywna";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // generateNumber
+            // 
+            this.generateNumber.Location = new System.Drawing.Point(458, 143);
+            this.generateNumber.Maximum = new decimal(new int[] {
+            2000000,
+            0,
+            0,
+            0});
+            this.generateNumber.Name = "generateNumber";
+            this.generateNumber.Size = new System.Drawing.Size(120, 23);
+            this.generateNumber.TabIndex = 5;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(458, 193);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(120, 23);
+            this.generateButton.TabIndex = 6;
+            this.generateButton.Text = "Generuj";
+            this.generateButton.UseVisualStyleBackColor = true;
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Location = new System.Drawing.Point(382, 277);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(34, 15);
+            this.timeLabel.TabIndex = 7;
+            this.timeLabel.Text = "Czas:";
+            // 
+            // sortInsert
+            // 
+            this.sortInsert.Location = new System.Drawing.Point(302, 351);
+            this.sortInsert.Name = "sortInsert";
+            this.sortInsert.Size = new System.Drawing.Size(75, 23);
+            this.sortInsert.TabIndex = 8;
+            this.sortInsert.Text = "SI";
+            this.sortInsert.UseVisualStyleBackColor = true;
+            this.sortInsert.Click += new System.EventHandler(this.sortInsert_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(383, 351);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "SM";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(464, 351);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "SQ";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // separatorSelected
+            // 
+            this.separatorSelected.FormattingEnabled = true;
+            this.separatorSelected.Items.AddRange(new object[] {
+            ";",
+            ",",
+            "spacja"});
+            this.separatorSelected.Location = new System.Drawing.Point(316, 46);
+            this.separatorSelected.Name = "separatorSelected";
+            this.separatorSelected.Size = new System.Drawing.Size(121, 23);
+            this.separatorSelected.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(327, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(101, 15);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Wybierz separator";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.separatorSelected);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.sortInsert);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.generateNumber);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.wynik);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.generateNumber)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private TextBox wynik;
+        private Button button2;
+        private CheckBox checkBox1;
+        private NumericUpDown generateNumber;
+        private Button generateButton;
+        private Label timeLabel;
+        private Button sortInsert;
+        private Button button5;
+        private Button button6;
+        private ComboBox separatorSelected;
+        private Label label2;
+    }
+}
---
+++ AiSD_IO2_cw4/Form1.cs
@@ -0,0 +1,224 @@
+﻿namespace AiSD_IO2_cw4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            generateNumber.Enabled = false;
+            generateButton.Enabled = false;
+            checkBox1.Checked = true;
+            separatorSelected.SelectedText = ";";
+            separatorSelected.Text = ";";
+        }
+
+        public void Swap(List<int> arr, int index1, int index2)
+        {
+            int element1 = arr[index1];
+
+            arr[index1] = arr[index2];
+            arr[index2] = element1;
+        }
+
+        // Bubbling sort
+        public double SortFun(List<int> arr)
+        {
+            DateTime timeStart = DateTime.Now;
+            bool cbz = false;
+            for (int i = 0; i < arr.Count - 1; i++)
+            {
+                for (int j = 0; j < arr.Count - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    { 
+                        Swap(arr, j, j + 1);
+                        cbz = true;
+                    }
+                }
+                if (!cbz)
+                    break;
+                else
+                    cbz = false;
+
+            }
+            DateTime timeEnd = DateTime.Now;
+            TimeSpan time = timeEnd - timeStart;
+            var seconds = time.TotalMilliseconds;
+            return seconds;
+        }
+
+        public double SelectionSort(List<int> arr)
+        {
+            DateTime timeStart = DateTime.Now;
+            for (int i = 0; i < arr.Count - 1; i++)
+            {
+                int min = i;
+                for (int j = i + 1; j < arr.Count; j++)
+                {
+                    if (arr[j] < arr[min])
+                        min = j;
+                }
+                Swap(arr, min, i);
+            }
+            DateTime timeEnd = DateTime.Now;
+            TimeSpan time = timeEnd - timeStart;
+            var seconds = time.TotalMilliseconds;
+            return seconds;
+        }
+
+        public double InsertionSort(List<int> arr)
+        {
+            DateTime timeStart = DateTime.Now;
+            for (int i = 0; i < arr.Count; i++)
+            {
+                int actualValue = arr[i];
+                int j = i - 1;
+                while(j >= 0 && arr[j] > actualValue)
+                {
+                    Swap(arr, j + 1, j);
+                    j--;
+                }
+                arr[j + 1] = actualValue;
+            }
+            DateTime timeEnd = DateTime.Now;
+            TimeSpan time = timeEnd - timeStart;
+            var seconds = time.TotalMilliseconds;
+            return seconds;
+        }
+
+        public void MergeSort(List<int> arr)
+        {
+
+        }
+
+        public void QuickSort(List<int> arr)
+        {
+            DateTime timeStart = DateTime.Now;
+        }
+
+        // Bubbling sort Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string separator = separatorSelected.Text == "spacja" ? " " : separatorSelected.Text;
+            if (textBox1.Text.Contains(separator))
+            {
+                string numbers = textBox1.Text;
+                numbers = numbers.Replace(" ", string.Empty);
+                string[] numbersArr = numbers.Split(separator);
+                List<int> list = new List<int>();
+
+                foreach (string number in numbersArr)
+                {
+                    list.Add(int.Parse(number));
+                }
+
+                double time = SortFun(list);
+
+                string res = "";
+                for (int i = 0; i < list.Count; i++)
+                {
+
+                    string actualValue = i < list.Count - 1 ? "" + list.ElementAt(i) + ", " : "" + list.ElementAt(i);
+                    res += actualValue;
+                }
+
+                wynik.Text = res;
+                timeLabel.Text = time + "ms";
+            } else
+            {
+                MessageBox.Show("Wybierz poprawny separator");
+            }
+            
+        }
+
+        // SS button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string separator = separatorSelected.Text == "spacja" ? " " : separatorSelected.Text;
+            if (textBox1.Text.Contains(separator))
+            {
+                string numbers = textBox1.Text;
+                numbers = separatorSelected.Text == "spacja" ? numbers : numbers.Replace(" ", string.Empty);
+                string[] numbersArr = numbers.Split(separator);
+                List<int> list = new List<int>();
+
+                foreach (string number in numbersArr)
+                {
+                    list.Add(int.Parse(number));
+                }
+
+                double time = SelectionSort(list);
+
+                string res = "";
+                for (int i = 0; i < list.Count; i++)
+                {
+
+                    string actualValue = i < list.Count - 1 ? "" + list.ElementAt(i) + ", " : "" + list.ElementAt(i);
+                    res += actualValue;
+                }
+
+                timeLabel.Text = time + "ms";
+                wynik.Text = res;
+            } else
+            {
+                MessageBox.Show("Wybierz poprawny separator");
+            }
+            
+        }
+
+        // Active left or right
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if(checkBox1.Checked)
+            {
+                textBox1.Enabled = true;
+                generateNumber.Enabled = false;
+                generateButton.Enabled = false;
+            }
+            else
+            {
+                textBox1.Enabled = false;
+                generateNumber.Enabled = true;
+                generateButton.Enabled = true;
+            }
+        }
+
+        private void sortInsert_Click(object sender, EventArgs e)
+        {
+            string separator = separatorSelected.Text == "spacja" ? " " : separatorSelected.Text;
+            if (textBox1.Text.Contains(separator))
+            {
+                string numbers = textBox1.Text;
+                numbers = separatorSelected.Text == "spacja" ? numbers : numbers.Replace(" ", string.Empty);
+                string[] numbersArr = numbers.Split(separator);
+                List<int> list = new List<int>();
+
+                foreach (string number in numbersArr)
+                {
+                    list.Add(int.Parse(number));
+                }
+
+                double time = InsertionSort(list);
+
+                string res = "";
+                for (int i = 0; i < list.Count; i++)
+                {
+
+                    string actualValue = i < list.Count - 1 ? "" + list.ElementAt(i) + ", " : "" + list.ElementAt(i);
+                    res += actualValue;
+                }
+
+                timeLabel.Text = time + "ms";
+                wynik.Text = res;
+            }
+            else
+            {
+                MessageBox.Show("Wybierz poprawny separator");
+            }
+        }
+    }
+}
