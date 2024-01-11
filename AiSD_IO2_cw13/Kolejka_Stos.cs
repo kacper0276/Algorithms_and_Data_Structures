46909d4ff4f61f792cff93ff72cd3210779b34c8
kacper0276 <kacper0276@op.pl>
2024-01-11 16:20:30 +0100

    Pojęcia z zajęć z dnia 11_01_2024

---
+++ AiSD_IO2_cw13/Kolejka_Stos.cs
@@ -0,0 +1,19 @@
+﻿namespace AiSD_IO2_cw5
+{
+    public class Kolejka_Stos
+    {
+        // Stos
+
+        // FIFO ( first in first out)
+
+        // LIFO ( last in first out)
+
+        // Kolejka
+
+        // Kolejki priorytetowe
+
+        // Listy (poprzednik, następnik, head, ogon), lista dwukierunkowa, dodawanie
+
+        // Tablice - czemu od 0 indexu, dlaczego nie można rozszerzać tablic
+    }
+}
